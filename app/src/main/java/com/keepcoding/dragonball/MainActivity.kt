cd92ff3fcbca75bad1df4e16c3981f2ec9f2a9b1
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-14 14:29:57 +0100

    Probando LOGS

--- app/src/main/java/com/keepcoding/dragonball/MainActivity.kt
+++ app/src/main/java/com/keepcoding/dragonball/MainActivity.kt
@@ -2,10 +2,16 @@ package com.keepcoding.dragonball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //println("hola")
+        //Misma manera de hacer un LOG
+        //Log.v("MainActivity", "onCreateFinished")
+        Log.v(MainActivity::javaClass.name, "onCreateFinished")
     }
 }
