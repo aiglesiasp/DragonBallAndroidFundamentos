1d49292a88b06b72951ed8adc9f8c1c6bd4e4a09
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-19 13:41:00 +0100

    Agregado Preferences de Tiempo

--- app/src/main/java/com/keepcoding/dragonball/MainActivity.kt
+++ app/src/main/java/com/keepcoding/dragonball/MainActivity.kt
@@ -1,18 +1,65 @@
 package com.keepcoding.dragonball
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.w(MainActivity::javaClass.name, "onCreate STARTING...")
-        setContentView(R.layout.login_constraint)
 
-        //println("hola")
-        //Misma manera de hacer un LOG
-        //Log.v("MainActivity", "onCreateFinished")
-        Log.d(MainActivity::javaClass.name, "onCreate FINISHED...")
+    // Clear variables para guardar el tiempo
+    private var tiempoInicio = 0L
+
+    companion object {
+        const val  TAG_TEMP = "Tiempo"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        println("ENTRANDO EN EL ONCREATE")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_constraint)
+    }
+
+    override fun onStart() {
+        println("ENTRANDO EN EL ONSTART")
+        super.onStart()
+        tiempoInicio = System.currentTimeMillis()
+        println("")
+    }
+
+    override fun onResume() {
+        println("ENTRANDO EN EL ONRESUME")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        println("ENTRANDO EN EL ONPAUSE")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        println("ENTRANDO EN EL ONSTOP")
+        val tiempoFin = System.currentTimeMillis()
+        val tiempoEnLaApp = tiempoFin - tiempoInicio
+
+        val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+
+        val tiempoAnterior = sharedPreferences.getLong(TAG_TEMP, 0L)
+
+        val editPreferences = sharedPreferences.edit()
+        editPreferences.putLong(TAG_TEMP, tiempoEnLaApp + tiempoAnterior)
+        editPreferences.apply()
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        println("ENTRANDO EN EL ONRESTART")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        println("ENTRANDO EN EL ONDESTROY")
+        super.onDestroy()
     }
 }
